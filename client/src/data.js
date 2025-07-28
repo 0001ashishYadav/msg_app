d2a12f1085f6fa60263a595984f489180c0e1ea8
000ashishYadav <000ashishyadav2003@gmail.com>
2025-07-29 02:05:13 +0530

    make data.js file for store all dat in a seperate file

---
+++ client/src/data.js
@@ -0,0 +1,30 @@
+export const messagesData = [
+  {
+    id: 1,
+    text: "Hey! How's your day going?",
+    sender: "other",
+    timestamp: "10:30 AM",
+    avatar:
+      "https://images.unsplash.com/photo-1494790108755-2616b612b786?w=40&h=40&fit=crop&crop=face",
+  },
+  {
+    id: 2,
+    text: "Pretty good! Just working on some new designs. What about you?",
+    sender: "me",
+    timestamp: "10:32 AM",
+  },
+  {
+    id: 3,
+    text: "That sounds awesome! I'd love to see what you're working on. I'm just catching up on some reading.",
+    sender: "other",
+    timestamp: "10:35 AM",
+    avatar:
+      "https://images.unsplash.com/photo-1494790108755-2616b612b786?w=40&h=40&fit=crop&crop=face",
+  },
+  {
+    id: 4,
+    text: "Sure! I'll share some screenshots later today.",
+    sender: "me",
+    timestamp: "10:37 AM",
+  },
+];
