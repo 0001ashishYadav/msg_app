9e1546e9c825b5021009f2fc5200135773714f38
000ashishYadav <000ashishyadav2003@gmail.com>
2025-08-02 14:28:32 +0530

    make context folder

--- client/src/context/context.jsx
+++ client/src/context/context.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { messagesData } from "./data";
+import { messagesData } from "../data";
 
 export const Context = createContext();
 
