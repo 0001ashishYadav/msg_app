b45474d9872bbccc0a9c855d052b09f172c31636
000ashishYadav <000ashishyadav2003@gmail.com>
2025-08-04 20:25:40 +0530

    define deffernt layout for auth pages and message page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,55 @@
-import { Route, Router } from "react-router";
-import AuthPage from "./pages/AuthPage";
+import { Route, Routes } from "react-router";
 import MessagePage from "./pages/MessagePage";
 import ProtectedRote from "./components/auth/ProtectedRote";
+import AuthSystem from "./pages/AuthSystem";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import { use, useState } from "react";
+import AuthLayout from "./layout/AuthLayout";
+import { useContextData } from "./context/context";
 
 function App() {
+  const { isLoading } = useContextData();
   return (
     <>
-      <Router>
-        <Route path="/authPage" element={<AuthPage />} />
+      {isLoading && <LoadingSpinner />}
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <AuthLayout>
+              <LoginPage />
+            </AuthLayout>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <AuthLayout>
+              <SignUpPage />
+            </AuthLayout>
+          }
+        />
+        <Route
+          path="/forgot"
+          element={
+            <AuthLayout>
+              <ForgotPasswordPage />
+            </AuthLayout>
+          }
+        />
+        <Route
+          path="/reset"
+          element={
+            <AuthLayout>
+              <ResetPasswordPage />
+            </AuthLayout>
+          }
+        />
+
+        <Route path="/auth" element={<AuthSystem />} />
         <Route
           path="/messagePage"
           element={
@@ -16,7 +58,7 @@ function App() {
             </ProtectedRote>
           }
         />
-      </Router>
+      </Routes>
     </>
   );
 }
